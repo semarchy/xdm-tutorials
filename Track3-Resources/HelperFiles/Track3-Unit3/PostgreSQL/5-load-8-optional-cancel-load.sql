73f08e39122760b714f490fed2609b869eb00249
Anna <anna@semarchy.com>
2018-09-18 17:03:13 -0700

    Port over queries from Oracle to PostgreSQL

---
+++ Track3-Resources/HelperFiles/Track3-Unit3/PostgreSQL/5-load-8-optional-cancel-load.sql
@@ -0,0 +1,16 @@
+/*
+ * Step 3: Cancel Load
+ * Call the function cancel_load to cancel the external load.
+ * You will need to clean up the data in the source table.
+ * Run as semarchy_repository user.
+ */
+do
+$$
+
+begin
+  perform semarchy_repository.cancel_load (
+       /* load_id */          /* Load ID returned by get_new_loadid */
+      ,'manual_etl_user'      /* Must exactly match the value used in get_new_loadid */
+  );
+end;
+$$ language plpgsql;
