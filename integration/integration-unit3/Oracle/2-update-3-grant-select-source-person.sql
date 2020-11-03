3f6960d3bd0e3c3fd563b3b693b139b3d3d2db84
cedricblanc <cedric.blanc@semarchy.com>
2020-11-03 12:32:09 +0100

    Removing old files (unit-xxx)

--- integration/integration-unit3/Oracle/2-update-3-grant-select-source-person.sql
+++
@@ -1,6 +0,0 @@
-/*
- * Allow SEMARCHY_CUSTOMER_B2C_MDM to select from the SOURCE_PERSON table.
- * Without the grants, the insert will fail due to lack of privileges.
- * Run this grant as the SYSTEM or admin user.
- */
-grant SELECT on SEMARCHY_STG.SOURCE_PERSON to SEMARCHY_CUSTOMER_B2C_MDM;
--- integration/integration-unit3/Oracle/5-load-1-semql-customers-without-products.sql
+++
@@ -1,4 +0,0 @@
-/* SemQL expression to search for customers who don't have any products.
- * Run in the Customer Business View using the SemQL filter type.
- */
-PersonType = 'CUSTOMER' AND NOT ANY PersonProducts HAVE ( ID is not null )
