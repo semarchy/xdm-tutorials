c443699d86d6f9924b9237938493f7e8c98122fd
cedricblanc <cedric.blanc@semarchy.com>
2021-05-17 18:47:41 +0200

    fix(Quick Start > On Premises)
    Add comments in the setenv files

--- quick-install/install-on-premises/setenv.sh
+++ quick-install/install-on-premises/setenv.sh
@@ -1,4 +1,10 @@
 CATALINA_OPTS="$CATALINA_OPTS -Dxdm.show.stacktrace.jsp.error=true -server -XX:+UseG1GC -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Slf4jLogger -Duser.language=en -Duser.country=US -Djava.awt.headless=true"
+
+#Configuration file
 CATALINA_OPTS="$CATALINA_OPTS -Dxdm.config.file=../conf/config.properties"
+
+#JAVA_HOME
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home
+
+#Setup Token (initial setup only)
 export SEMARCHY_SETUP_TOKEN=mySetupToken
