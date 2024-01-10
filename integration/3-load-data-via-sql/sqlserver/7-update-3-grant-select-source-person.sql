b23cde6132b3b17b63e43ee4486f157982ad1162
Valentina Antonova <valentina.antonova@semarchy.com>
2024-01-10 11:41:12 +0100

    Integration tutorials update

--- integration/3-load-data-via-sql/sqlserver/7-update-3-grant-select-source-person.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Allow semarchy_customer_b2c_mdm to select from the source_person table.
- * Without the grants, the insert will fail due to lack of privileges.
- * Run this grant as the postgres or admin user.
- */
-use semarchy_stg;
-grant select on dbo.source_person to semarchy_customer_b2c_mdm;
